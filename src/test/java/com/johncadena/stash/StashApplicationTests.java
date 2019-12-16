a192e8adc875b9716d4ca9e76923b6db82cff89b
John Cadena <cadena.john.85@gmail.com>
2019-12-15 20:47:40 -0600

    create new repository

---
+++ src/test/java/com/johncadena/stash/StashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johncadena.stash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StashApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
