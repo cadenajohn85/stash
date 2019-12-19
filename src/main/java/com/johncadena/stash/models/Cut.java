5b535d1e9d5fb6ead8d41706e4835feb24076ebc
John Cadena <cadena.john.85@gmail.com>
2019-12-19 13:58:47 -0600

    add Cut model

---
+++ src/main/java/com/johncadena/stash/models/Cut.java
@@ -0,0 +1,91 @@
+package com.johncadena.stash.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="cuts")
+public class Cut {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String fabricName;
+
+    @Column(nullable = false)
+    private double quantity;
+
+    @Column(length = 500)
+    private String description;
+
+    @Column(length = 100)
+    private String line;
+
+    @Column(length = 100)
+    private String brand;
+
+    @Column(length = 100)
+    private String designer;
+
+    //type (yardage, fat quarter, layer cake, charm pack, jelly roll,
+    //image (optional)
+
+    public Cut() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFabricName() {
+        return fabricName;
+    }
+
+    public void setFabricName(String fabricName) {
+        this.fabricName = fabricName;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLine() {
+        return line;
+    }
+
+    public void setLine(String line) {
+        this.line = line;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getDesigner() {
+        return designer;
+    }
+
+    public void setDesigner(String designer) {
+        this.designer = designer;
+    }
+}
--- src/main/java/com/johncadena/stash/models/Stash.java
+++ src/main/java/com/johncadena/stash/models/Stash.java
@@ -1,6 +1,7 @@
 package com.johncadena.stash.models;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name="stashes")
@@ -14,11 +15,36 @@ public class Stash {
     private User user;
 
         // One to one: user to stash
-    // One to many: stash to cuts
+        // One to many: stash to cuts
     // Many to many: stashes to projects
 
-//    @OneToMany(mappedBy = "cut")
-//    private List<Cut> cuts;
+    @OneToMany(mappedBy = "fabricName")
+    private List<Cut> cuts;
 
+    public Stash() {
+    }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Cut> getCuts() {
+        return cuts;
+    }
+
+    public void setCuts(List<Cut> cuts) {
+        this.cuts = cuts;
+    }
 }
